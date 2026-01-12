d31af12b6e93b6beede26d4a6c815bce44ec9dce
imani-prog <timothyimani128@gmail.com>
2026-01-12 03:58:56 +0300

    Refactored positioning fior the Paginantion component

--- src/Components/Admin/AdminSidebar.jsx
+++ src/Components/Admin/AdminSidebar.jsx
@@ -1,5 +1,5 @@
 import { NavLink } from 'react-router-dom';
-import logo from '../assets/mediLink.png';
+import logo from '../../assets/mediLink.png';
 
 const sidebarLinks = [
   { to: '/admin/dashboard', label: 'Dashboard', icon: (
